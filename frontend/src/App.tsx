51b599666f4d10dcf15c123a9dc4ba7e2c2ebdd2
lazypupu <zcatnico@gmail.com>
2023-01-11 22:35:18 +0800

    minimal POC

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import MyBox from "./app/test";
+
+function App() {
+    return (
+        <div className="App" id="root">
+            <MyBox></MyBox>
+            <h1>
+                Work In Progress.
+            </h1>
+        </div>
+    );
+}
+
+export default App;
